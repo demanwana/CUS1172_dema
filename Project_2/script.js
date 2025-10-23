9d9ff7f2d09694f64bbddbebaf9efb1958cea9b9
demanwana <dema.nwana@gmail.com>
2025-10-23 00:19:10 -0400

    Remove empty line at the start of script.js
    Removed unnecessary empty line at the beginning of the script.

--- Project_2/script.js
+++ Project_2/script.js
@@ -1,4 +1,3 @@
-
 // Retrieve todo from local storage or initialize an empty array
 let todo = JSON.parse(localStorage.getItem("todo")) || [];
 const todoInput = document.getElementById("todoInput");
